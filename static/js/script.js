008c505dfc9a14775b91b089308e9675a5bd841f
sarahwong <71740977+Wong-S@users.noreply.github.com>
2021-02-20 18:35:50 -0600

    Restore old commits

--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,8 @@
-// import * as Tone from 'tone'
+import * as Tone from 'tone'
 
 //create a synth and connect it to the main output (your speakers)
-// const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination();
 
-// //play a middle 'C' for the duration of an 8th note
-// synth.triggerAttackRelease("C4", "8n");
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
 
-
-// document.querySelector(".key-white").addEventListener("click", async () => {
-//     const synth = new Tone.Synth().toDestination();
-//     await Tone.start();
-//     synth.triggerAttackRelease("C4", "8n");
-//     alert("You cliecked me");
-//     console.log("works?");
-// })
-
-// let elementKey = document.getElementById("C-sharp");
-// console.log("HI")
-// console.log(elementKey)
-
-// let Key = document.querySelectorAll("#C-sharp")
-// console.log(Key);
-
-// function callMe(evt) {
-//     Key.addEventListener("click", (evt) => {
-
-//     alert("You cliecked me");
-//     console.log("works?");
-// });
-
-// callMe();
-let Key = document.getElementById("hi")
-// document.getElementById("#hi").addEventListener("click", myFunction);
-
-function myFunction() {
-    Key.addEventListener("click", (evt) => {
-        alert("Hello World")
-    });
-}
-
-myFunction() 
